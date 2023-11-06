e9a22b1251004fb11bd6f7f705c68e9f7927eaa0
VHSTapeDVD <axschoenning@gmail.com>
2023-11-06 14:58:25 +0100

    Working Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private CharacterController controller;
+    
+
+
+    private int desiredLane = 1; // upper lane = 0, middel lane = 1, lower lane = 2
+    public float laneDistance = 4; // the length between lanes
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+     
+        }
         
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+
+        }
+
+        // Calculation of new position
+
+        Vector3 newPosition = transform.position.z * transform.position.y * transform.up;
+
+        if (desiredLane == 0)
+        {
+            newPosition += Vector3.down * laneDistance;
+        } 
+        else if (desiredLane == 2) 
+        {
+            newPosition += Vector3.up * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position,newPosition,80*Time.deltaTime);
     }
+
+  
+
 }
