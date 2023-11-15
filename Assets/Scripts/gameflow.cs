dac2319437da9202ed140f737187d662ca5de744
Tristan <57353353+fanteck-boop@users.noreply.github.com>
2023-11-15 13:38:40 +0100

    Færdig gameflow.cs
    Bedre regulering af spawn
    
    Signed-off-by: Tristan <57353353+fanteck-boop@users.noreply.github.com>

--- Assets/Scripts/gameflow.cs
+++ Assets/Scripts/gameflow.cs
@@ -1,31 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class gameflow : MonoBehaviour
 {
     public Transform goobleObj;
+    public Transform plasticObj;
     public int randYcoor;
-    public string spawnDelay = "n";
+    private bool isGooble = false; // Track which object to spawn
+    public int spawnLimit = 5;
+    public int extraPlasticEvery = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnObjects());
     }
 
-        // Update is called once per frame
-        void Update()
-        {
-            if (spawnDelay == "n") { 
-            randYcoor = Random.Range (0, 3);
-                Instantiate(goobleObj, new Vector3(15, randYcoor, 0), goobleObj.rotation);
-                spawnDelay = "y";
-                StartCoroutine (resetSpawnTimer ());
-            }
-        }
-    IEnumerator resetSpawnTimer()
+    IEnumerator SpawnObjects()
     {
-        yield return new WaitForSeconds (2);
-        spawnDelay = "n";
+        while (true)
+        {
+            if (isGooble)
+            {
+                randYcoor = Random.Range(0, 3);
+                Instantiate(goobleObj, new Vector3(15, randYcoor, 0), goobleObj.rotation);
+            }
+            else
+            {
+                // Spawn 6 plasticObj in a row
+                for (int i = 0; i < spawnLimit; i++)
+                {
+                    randYcoor = Random.Range(0, 3);
+                    Instantiate(plasticObj, new Vector3(16, randYcoor, 0), plasticObj.rotation);
+                    yield return new WaitForSeconds(2);
+
+                    // Spawn an extra plasticObj every 3 iterations
+                    if ((i + 1) % extraPlasticEvery == 0)
+                    {
+                        randYcoor = Random.Range(0, 3);
+                        Instantiate(plasticObj, new Vector3(16, randYcoor, 0), plasticObj.rotation);
+                    }
+                }
+            }
+
+            isGooble = !isGooble; // Switch between goobleObj and plasticObj
+            yield return new WaitForSeconds(2);
+        }
     }
-    }
+}
