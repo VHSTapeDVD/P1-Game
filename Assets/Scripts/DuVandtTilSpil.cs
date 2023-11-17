a7db72c1e574dd722aeaf1c307c01ae62bf6375e
atafre23 <147533548+atafre23@users.noreply.github.com>
2023-11-17 09:19:29 +0100

    Add files via upload
    Signed-off-by: atafre23 <147533548+atafre23@users.noreply.github.com>

---
+++ Assets/Scripts/DuVandtTilSpil.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goodgameover: MonoBehaviour
+{
+    public void Duvandt()
+
+    {
+        SceneManager.LoadScene("Flappy Bird");
+    }
+}
